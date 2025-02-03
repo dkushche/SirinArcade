0d67444cbd627fe26c93f28fd377745daff9dd98
Danyl Davydiuk <d.davydiuk@sirinsoftware.com>
2025-02-04 00:54:34 +0200

    holy c, i dont understand..

--- sirin_arcade/core/src/main.c
+++ sirin_arcade/core/src/main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 
 #include <sirin_arcade/render.h>
+#include <sirin_arcade/sound.h>
 
 //to remove
 #include <alsa/asoundlib.h>
@@ -9,155 +10,188 @@
 #include <sndfile.h>
 #include <time.h>
 #include <stdio.h>
+#include <stdatomic.h>
+#include <sched.h>
+
+/**
+* ~/.asoundrc
+
+pcm.!default {
+	type plug
+	slave.pcm "dmixer"
+}
+
+pcm.dmixer  {
+ 	type dmix
+ 	ipc_key 1024
+	slave.pcm "hw:0,0"
+}
+
+ctl.dmixer {
+	type hw
+	card 0
+}
+*/
+
 
 #define BUFFER_SIZE 4096
 
 typedef struct {
-    snd_pcm_t *pcm_handle;
+	int restart_on_end; // 1 yes, whatever else no
+	atomic_bool stop_rn; // 1 yes
+    char* path;
 } thread_arg_t;
 
-void* hell(void *arg) {
-	thread_arg_t *data = (thread_arg_t *)arg;
-
-    snd_pcm_t *pcm_handle = data->pcm_handle;
-    snd_pcm_hw_params_t *params;
-    int dir;
-
+void start_playing(void *arg) {
+	thread_arg_t *given_to_thread_data = (thread_arg_t *)arg;
+    snd_pcm_t *pcm_handle;
     snd_pcm_uframes_t frames = BUFFER_SIZE;
-
-    //
     SF_INFO sfinfo;
-    SNDFILE *sndfile = sf_open("/access_point/example.wav", SFM_READ, &sfinfo);
+    SNDFILE *sndfile;
+
+    while (snd_pcm_open(&pcm_handle, "default", SND_PCM_STREAM_PLAYBACK, SND_PCM_ASYNC) < 0) {
+        sched_yield();
+    }
+
+   	sndfile = sf_open(given_to_thread_data->path, SFM_READ, &sfinfo);
     if (!sndfile) {
-        fprintf(stderr, "Не вдалося відкрити файл: %s\n", sf_strerror(NULL));
+        fprintf(stderr, "Failure with file opening\n");
         exit(1);
     }
+
     int channels = sfinfo.channels;
     unsigned int rate = (unsigned int) sfinfo.samplerate;
-    short *buffer = malloc(BUFFER_SIZE * channels * sizeof(short)); // Буфер для зчитування аудіо
 
-    // Встановлюємо параметри
+    snd_pcm_hw_params_t *params;
+
     snd_pcm_hw_params_alloca(&params);
+    if (snd_pcm_hw_params_any(pcm_handle, params) < 0) {
+    	fprintf(stderr, "Cannot initialize hardware parameters\n");
+    	exit(1);
+	}
+	if (snd_pcm_hw_params_set_access(pcm_handle, params, SND_PCM_ACCESS_RW_INTERLEAVED) < 0) {
+    	fprintf(stderr, "Error setting access type\n");
+    	exit(1);
+	}
+	if (snd_pcm_hw_params_set_format(pcm_handle, params, SND_PCM_FORMAT_S16_LE) < 0) {
+	    fprintf(stderr, "Error setting sample format\n");
+    	exit(1);
+	}
+	if (snd_pcm_hw_params_set_rate(pcm_handle, params, rate, 0) < 0) {
+    	fprintf(stderr, "Error setting sample rate\n");
+    	exit(1);
+	}
+	if (snd_pcm_hw_params_set_channels(pcm_handle, params, channels) < 0) {
+	    fprintf(stderr, "Error setting channel count\n");
+	    exit(1);
+	}
+//    snd_pcm_hw_params_set_period_size_near(pcm_handle, params, &frames, &dir);
 
-    snd_pcm_hw_params_any(pcm_handle, params);
-    snd_pcm_hw_params_set_access(pcm_handle, params, SND_PCM_ACCESS_RW_INTERLEAVED);
-    snd_pcm_hw_params_set_format(pcm_handle, params, SND_PCM_FORMAT_S16_LE);
-    snd_pcm_hw_params_set_rate_near(pcm_handle, params, &rate, &dir);
-    snd_pcm_hw_params_set_channels(pcm_handle, params, channels);  // 2 канали (стерео)
-    snd_pcm_hw_params_set_period_size_near(pcm_handle, params, &frames, &dir);
-    snd_pcm_hw_params(pcm_handle, params);
+	printf("Sample rate: %d, Channels: %d, Format: %d\n", sfinfo.samplerate, sfinfo.channels, sfinfo.format);
+
     if (snd_pcm_hw_params(pcm_handle, params) < 0) {
         fprintf(stderr, "Error setting HW params\n");
         exit(1);
     }
-    clock_t start_time;
-    start_time = clock();
 
-    // Відтворення звуку
+    short *buffer = malloc(BUFFER_SIZE * channels * sizeof(short));
+
     while (1) {
-        double time_taken = (((double)(clock() - start_time))/CLOCKS_PER_SEC); // in seconds
-        if (time_taken >= 10.0) {
-            start_time = clock();
-            if (sf_seek(sndfile, 0, SEEK_SET) == -1) {
-              fprintf(stderr, "setting audio to start failed \n");
-              exit(0);
-            }
-            memset(buffer, 0, BUFFER_SIZE * channels * sizeof(short));
-            snd_pcm_drop(pcm_handle); // Зупиняємо відтворення
-            int err = snd_pcm_prepare(pcm_handle); // Готуємо пристрій знову
-			if (err < 0) {
-            	fprintf(stderr, "wtf, prepare failed: %s\n", snd_strerror(err));
-            	exit(0);
-            }
-        }
+      	if (atomic_load(&given_to_thread_data->stop_rn) == 1) {
+			break;
+      	}
         int read_frames = sf_read_short(sndfile, buffer, frames * channels);
 
-        // Якщо кінець файлу, зупиняємо відтворення
         if (read_frames == 0) {
+           	if (given_to_thread_data->restart_on_end != 1) {
+                  break;
+            }
+            if (sf_seek(sndfile, 0, SEEK_SET) == -1) {
+            	fprintf(stderr, "setting audio to start failed \n");
+            	exit(1);
+        	}
 
-            break;
+            memset(buffer, 0, BUFFER_SIZE * channels * sizeof(short));
+
+            snd_pcm_drop(pcm_handle);
+            if (snd_pcm_prepare(pcm_handle) < 0) {
+            	fprintf(stderr, "snd_pcm_prepare failed\n");
+            	exit(1);
+            }
         }
 
-        // Відправляємо дані до пристрою
         int err = snd_pcm_writei(pcm_handle, buffer, read_frames / channels);
 		if (err == -EPIPE) {
-    		fprintf(stderr, "XRUN occurred\n");
-    		snd_pcm_prepare(pcm_handle);
-		} else if (err < 0) {
-    		fprintf(stderr, "Error writing to PCM device: %s\n", snd_strerror(err));
-    		break;
-		}
-
+    	    fprintf(stderr, "XRUN occurred\n");
+    	    snd_pcm_prepare(pcm_handle);
+        } else if (err < 0) {
+   	    	fprintf(stderr, "Error writing to PCM device: %s\n", snd_strerror(err));
+   	    	break; //or exit idk
+	    }
     }
+
     free(buffer);
     sf_close(sndfile);
     free(arg);
-	return NULL;
+    snd_pcm_close(pcm_handle);
+}
+
+void *play_wave1(char *path, int cycled) {
+	pthread_t thread;
+    thread_arg_t *arg;
+
+    arg = (thread_arg_t *)malloc(sizeof(thread_arg_t));
+    if (arg == NULL) {
+        perror("Failed to allocate memory");
+        return NULL;
+    }
+
+    arg->restart_on_end = cycled;
+    arg->path = path;
+    arg->stop_rn = 0;
+
+    int result = pthread_create(&thread, NULL, start_playing, (void*)arg);
+
+	if (result != 0) {
+        printf("Error creating thread: %d\n", result);
+        return NULL;
+    }
+
+    if (cycled) {
+      	return (void*)arg;
+    } else {
+     	return NULL;
+    }
 }
 
 int main(void)
 {
-    screen_t *screen = initialze_screen();
-    if (screen == NULL)
-    {
-        return 1;
-    }
-
-    pixel_t *pixel = create_pixel('X', ARCADE_YELLOW);
-
-    // to move into lib
+//    screen_t *screen = initialze_screen();
+//    if (screen == NULL)
+//    {
+//        return 1;
+//    }
+//
+//    pixel_t *pixel = create_pixel('X', ARCADE_YELLOW);
 
 
-    // Відкриваємо пристрій
-    snd_pcm_t *pcm_handle;
-    while (snd_pcm_open(&pcm_handle, "hw:0,0", SND_PCM_STREAM_PLAYBACK, 0) < 0) {
-        fprintf(stderr, "Trying open audio device again\n");
-    }
-
-    pthread_t *thread;
-    thread_arg_t *arg = (thread_arg_t *)malloc(sizeof(thread_arg_t));
-    if (arg == NULL) {
-        perror("Failed to allocate memory");
-        return 1;
-    }
-    arg->pcm_handle = pcm_handle;
-    int result = pthread_create(thread, NULL, hell, (void*)arg);
-    //do not reuse arg, its now for that thread and not anyone else!
-
-	if (result != 0) {
-        // Перевірка на помилку
-        printf("Error creating thread: %d\n", result);
-        return 1;
-    }
-    sleep(5);
-
-    thread_arg_t *arg2 = (thread_arg_t *)malloc(sizeof(thread_arg_t));
-    if (arg2 == NULL) {
-        perror("Failed to allocate memory");
-        return 1;
-    }
-    arg2->pcm_handle = pcm_handle;
-    pthread_t *thread2;
-
-    pthread_create(thread2, NULL, hell, (void*)arg2);
-    //
-
-    while (1)
-    {
-        draw(screen, 0, 0, pixel);
-        draw(screen, 0, screen->width - 1, pixel);
-        draw(screen, screen->height - 1, 0, pixel);
-        draw(screen, screen->height / 2, screen->width / 2, pixel);
-        draw(screen, screen->height - 1, screen->width - 1, pixel);
-
-        render(screen);
-    }
-
-    free(pixel);
-
-    free_screen(screen);
-
-    snd_pcm_close(pcm_handle);
-
+    void *hahahhahahahha = play_wave("/access_point/example.wav", 1);
+    sleep(6);
+    void *hahahhahahah3ha = play_wave("/access_point/example.wav", 1);
+ 	while (1) {}
+//    while (1)
+//    {
+//        draw(screen, 0, 0, pixel);
+//        draw(screen, 0, screen->width - 1, pixel);
+//        draw(screen, screen->height - 1, 0, pixel);
+//        draw(screen, screen->height / 2, screen->width / 2, pixel);
+//        draw(screen, screen->height - 1, screen->width - 1, pixel);
+//
+//        render(screen);
+//    }
+//
+//    free(pixel);
+//
+//    free_screen(screen);
     return 0;
 }
--- sirin_arcade/sdk/src/sound.c
+++ sirin_arcade/sdk/src/sound.c
@@ -1,5 +1,151 @@
 #include "private/sound.h"
 
-void init_sound_device(void) {
+#ifndef SOUND1_H
+#define SOUND1_H
 
+#include <stddef.h>
+#include <stdlib.h>
+#include <alsa/asoundlib.h>
+#include <math.h>
+#include <sndfile.h>
+#include <time.h>
+#include <stdio.h>
+#include <stdatomic.h>
+#include <sched.h>
+#include <stdatomic.h>
+
+#endif //SOUND1_H
+
+#define BUFFER_SIZE 4096
+
+typedef struct {
+	int restart_on_end; // 1 yes, whatever else no
+	atomic_bool stop_rn; // 1 yes
+    char* path;
+} thread_arg_t;
+
+void start_playing(void *arg) {
+	thread_arg_t *given_to_thread_data = (thread_arg_t *)arg;
+    snd_pcm_t *pcm_handle;
+    snd_pcm_uframes_t frames = BUFFER_SIZE;
+    SF_INFO sfinfo;
+    SNDFILE *sndfile;
+
+    while (snd_pcm_open(&pcm_handle, "default", SND_PCM_STREAM_PLAYBACK, SND_PCM_ASYNC) < 0) {
+        sched_yield();
+    }
+
+   	sndfile = sf_open(given_to_thread_data->path, SFM_READ, &sfinfo);
+    if (!sndfile) {
+        fprintf(stderr, "Failure with file opening\n");
+        exit(1);
+    }
+
+    int channels = sfinfo.channels;
+    unsigned int rate = (unsigned int) sfinfo.samplerate;
+
+    snd_pcm_hw_params_t *params;
+
+    snd_pcm_hw_params_alloca(&params);
+    if (snd_pcm_hw_params_any(pcm_handle, params) < 0) {
+    	fprintf(stderr, "Cannot initialize hardware parameters\n");
+    	exit(1);
+	}
+	if (snd_pcm_hw_params_set_access(pcm_handle, params, SND_PCM_ACCESS_RW_INTERLEAVED) < 0) {
+    	fprintf(stderr, "Error setting access type\n");
+    	exit(1);
+	}
+	if (snd_pcm_hw_params_set_format(pcm_handle, params, SND_PCM_FORMAT_S16_LE) < 0) {
+	    fprintf(stderr, "Error setting sample format\n");
+    	exit(1);
+	}
+	if (snd_pcm_hw_params_set_rate(pcm_handle, params, rate, 0) < 0) {
+    	fprintf(stderr, "Error setting sample rate\n");
+    	exit(1);
+	}
+	if (snd_pcm_hw_params_set_channels(pcm_handle, params, channels) < 0) {
+	    fprintf(stderr, "Error setting channel count\n");
+	    exit(1);
+	}
+//    snd_pcm_hw_params_set_period_size_near(pcm_handle, params, &frames, &dir);
+
+	printf("Sample rate: %d, Channels: %d, Format: %d\n", sfinfo.samplerate, sfinfo.channels, sfinfo.format);
+
+    if (snd_pcm_hw_params(pcm_handle, params) < 0) {
+        fprintf(stderr, "Error setting HW params\n");
+        exit(1);
+    }
+
+    short *buffer = malloc(BUFFER_SIZE * channels * sizeof(short));
+
+    while (1) {
+      	if (atomic_load(&given_to_thread_data->stop_rn) == 1) {
+			break;
+      	}
+        int read_frames = sf_read_short(sndfile, buffer, frames * channels);
+
+        if (read_frames == 0) {
+           	if (given_to_thread_data->restart_on_end != 1) {
+                  break;
+            }
+            if (sf_seek(sndfile, 0, SEEK_SET) == -1) {
+            	fprintf(stderr, "setting audio to start failed \n");
+            	exit(1);
+        	}
+
+            memset(buffer, 0, BUFFER_SIZE * channels * sizeof(short));
+
+            snd_pcm_drop(pcm_handle);
+            if (snd_pcm_prepare(pcm_handle) < 0) {
+            	fprintf(stderr, "snd_pcm_prepare failed\n");
+            	exit(1);
+            }
+        }
+
+        int err = snd_pcm_writei(pcm_handle, buffer, read_frames / channels);
+		if (err == -EPIPE) {
+    	    fprintf(stderr, "XRUN occurred\n");
+    	    snd_pcm_prepare(pcm_handle);
+        } else if (err < 0) {
+   	    	fprintf(stderr, "Error writing to PCM device: %s\n", snd_strerror(err));
+   	    	break; //or exit idk
+	    }
+    }
+
+    free(buffer);
+    sf_close(sndfile);
+    free(arg);
+    snd_pcm_close(pcm_handle);
+}
+
+void *play_wave(char *path, int cycled) {
+	pthread_t thread;
+    thread_arg_t *arg;
+
+    arg = (thread_arg_t *)malloc(sizeof(thread_arg_t));
+    if (arg == NULL) {
+        perror("Failed to allocate memory");
+        return NULL;
+    }
+
+    arg->restart_on_end = cycled;
+    arg->path = path;
+    arg->stop_rn = 0;
+
+    int result = pthread_create(&thread, NULL, start_playing, (void*)arg);
+
+	if (result != 0) {
+        printf("Error creating thread: %d\n", result);
+        return NULL;
+    }
+
+    if (cycled) {
+      	return (void*)arg;
+    } else {
+     	return NULL;
+    }
+}
+
+void stop_wave(void *sound) { // to call only once for cycled waves, or else... write after free
+	atomic_store(&((thread_arg_t*)sound)->stop_rn, 1);
 }
