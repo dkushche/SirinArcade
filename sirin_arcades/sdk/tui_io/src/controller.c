c457adc8702f36f54f2274cf221a981da51c0e4c
Dima Kushchevskyi <d.kushchevskyi@sirinsoftware.com>
2025-03-03 08:53:05 +0200

    Improve controller

--- sirin_arcades/sdk/tui_io/src/controller.c
+++ sirin_arcades/sdk/tui_io/src/controller.c
@@ -5,45 +5,75 @@
 
 #include <tui_io.h>
 
-static char *keys_array = NULL;
-static size_t keys_array_capacity = 0;
-static size_t keys_array_length = 0;
+typedef struct vector vector_t;
 
-void add_to_array(char element) {
-    if (keys_array == NULL)
+typedef void realloc_handler(vector_t *self);
+typedef void add_handler(vector_t *self, char element, size_t *keys_array_length);
+
+typedef struct vector
+{
+    char *keys_array;
+    size_t capacity;
+
+    realloc_handler *realloc;
+    add_handler     *add;
+} vector_t;
+
+static void vector_add(vector_t *self, char element, size_t *keys_array_length)
+{
+    if (*keys_array_length == self->capacity)
     {
-        keys_array_capacity = 10;
-
-        keys_array = (char *)malloc(
-            keys_array_capacity * sizeof(char)
-        );
+        self->realloc(self);
     }
 
-    if (keys_array_length == keys_array_capacity)
-    {
-        keys_array_capacity = keys_array_capacity * 2;
+    self->keys_array[*keys_array_length] = element;
 
-        keys_array = (char *)realloc(
-            keys_array, keys_array_capacity * sizeof(char)
-        );
-    }
+    (*keys_array_length)++;
+}
 
-    keys_array[keys_array_length] = element;
-    keys_array_length = keys_array_length + 1;
+static void vector_realloc(vector_t *self)
+{
+    self->capacity = self->capacity * 2;
+
+    self->keys_array = (char *)realloc(
+        self->keys_array, self->capacity * sizeof(char)
+    );
+}
+
+static vector_t *vector_init(void)
+{
+    vector_t *res = (vector_t *)malloc(sizeof(vector_t));
+
+    res->capacity = 10;
+    res->keys_array = (char *)malloc(
+        res->capacity * sizeof(char)
+    );
+
+    res->realloc = vector_realloc;
+    res->add = vector_add;
+
+    return res;
 }
 
 char *get_keys(void)
 {
-    keys_array_length = 0;
+    static vector_t *vec = NULL;
+
+    size_t keys_array_length = 0;
     int ch;
+
+    if (vec == NULL)
+    {
+        vec = vector_init();
+    }
 
     while ((ch = getch()) != EOF) {
         if (ch == W || ch == A || ch == S || ch == D || ch == SPACE || ch == C) {
-            add_to_array(ch);
+            vec->add(vec, ch, &keys_array_length);
         }
     }
 
-    add_to_array(END);
+    vec->add(vec, END, &keys_array_length);
 
-    return keys_array;
+    return vec->keys_array;
 }
